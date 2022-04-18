b8b7b774b036965586177b05ca30aa0be5b26738
Tam-Git <47208145+Tam-Git@users.noreply.github.com>
2022-04-18 12:25:11 +0800

    Venue context part added
    Wait for testing after the release of the database part
    
    -API GET did not specify ID, don't know whether can get all the elements
    -HTML printing format not so sure, don't know if need print the name only or all informations in the venue

--- StudySafeTrace/data/views.py
+++ StudySafeTrace/data/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from datetime import date,timedelta
+from datetime import date, timedelta
 import requests
 import json
+import urllib
 
 # Create your views here.
+
+
 def venues(request):
 
-    context={
+    payload = urllib.parse.urlencode(
+        {"q": json.dumps({"resource": "https://young-lowlands-48457.herokuapp.com/venue"})})
+    venues_response = requests.get(
+        "https://young-lowlands-48457.herokuapp.com/venue", params=payload)
 
-    }
+    print("Venues response status: ", venues_response.status_code)
+    if (venues_response.status_code != 200):
+        return
+
+    venues_data = venues_response.json()
+
+    if (len(venues_data) == 0):
+        print("No venues data found!")
+        return
+
+    context = {"venues": venues_data}
 
     return render(request, 'venues.html', context=context)
+
 
 def contacts(request):
 
